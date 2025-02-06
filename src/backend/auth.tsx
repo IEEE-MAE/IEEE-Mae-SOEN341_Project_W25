59c6b89765c25afc75349cba0517db23cade40fc
Camila Kin Márquez Sosa <camilakinmarquezsosa@camis-laptop.wireless.concordia.ca>
2025-02-06 17:27:43 -0500

    login and signup authentication and navigation

---
+++ src/backend/auth.tsx
@@ -0,0 +1,54 @@
+import { db, auth } from "../config/firebase.tsx";
+import { doc, setDoc  } from "firebase/firestore"
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+} from "@firebase/auth";
+
+// from sign up page
+export const SignUpAuth= async (email : string, password : string, username :string)=>{
+    try{
+        // create account using firebase authentication
+        const {user} = await createUserWithEmailAndPassword(auth, email, password);
+
+        // store account's username in firebase collections/'users'
+        await setDoc(doc(db, "users", user.uid), {
+                displayName: username,
+                team: {},
+                role: ""
+        });
+        console.log("sign up: " + user.email);
+        return user;
+    }
+    catch(error){
+        console.log("Error during signup: " + error);
+        throw error;
+    }
+}
+
+// from log in page
+export const SignInAuth= async (email : string, password: string)=>{
+    try{
+        // log into account using firebase authentication
+        const {user} = await signInWithEmailAndPassword(auth, email, password);
+        console.log("sign in: " + user.email);
+        return user;
+    }
+    catch(error){
+        console.log("Error during sign in: " + error);
+        throw error;
+    }
+}
+
+// from wherever user can log out
+export const SignOutAuth= async ()=>{
+    try{
+        // logout using firebase
+        await auth.signOut();
+        console.log("sign out");
+    }
+    catch(error){
+        console.log("Error during sign out: " + error);
+        throw error;
+    }
+}
