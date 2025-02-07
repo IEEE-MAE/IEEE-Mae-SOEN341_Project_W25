36275be23ca89cc247ed420310e21bf3e26b1393
Camila Kin Márquez Sosa <camilakinmarquezsosa@camis-laptop.local>
2025-02-06 22:33:04 -0500

    added joinTeam and createTeam pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import SignUp from "./components/SignUp";
 import LogIn from "./components/LogIn";
 import JoinTeam from "./components/JoinTeam";
+import CreateTeam from "./components/CreateTeam";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App(){
@@ -11,6 +12,7 @@ function App(){
                 <Route path="/" element={<SignUp/>} />
                 <Route path="/LogIn" element={<LogIn/>} />
                 <Route path="/JoinTeam" element={<JoinTeam/>} />
+                <Route path="/CreateTeam" element={<CreateTeam/>} />
             </Routes>
         </Router>
     )
